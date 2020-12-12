871cd1fe88a812a99364c05350877643e8f5ce6a
liuxiaoxuan97 <54862568+liuxiaoxuan97@users.noreply.github.com>
2020-12-11 22:46:35 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 setup(
   name = 'FADiff',         # How you named your package folder (MyLib)
   packages=find_packages(),   # Chose the same as "name"
-  version = '0.18',      # Start with a small number and increase it with every change you make
+  version = '0.19',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'autodiff using forward and reverse mode',   # Give a short description about your library
   author = 'teamxvii',                   # Type in your name
   
   url = 'https://github.com/liuxiaoxuan97/FADiff',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/liuxiaoxuan97/FADiff/archive/v_18.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/liuxiaoxuan97/FADiff/archive/v_19.tar.gz',    # I explain this later on
   keywords = ['autodiff', 'forward mode', 'reverse mode'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
