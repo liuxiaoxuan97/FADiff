26c1dd23f76ba62ae264a11226baacfdb598ef35
liuxiaoxuan97 <54862568+liuxiaoxuan97@users.noreply.github.com>
2020-12-11 21:13:11 -0500

    Update __init__.py

--- FADiff/__init__.py
+++ FADiff/__init__.py
@@ -1,10 +1,6 @@
 from FADiff.FADiff import FADiff
-from fad.Gradients import Scal
-from fad.Matrices import Vect
-from rev.Gradients import Scal
-from rev.Matrices import Vect
-# from fad.Gradients import Scal as fadScal
-# from fad.Matrices import Vect as fadVect
-# from rev.Gradients import Scal as revScal
-# from rev.Matrices import Vect as revVect
+from FADiff.fad.Gradients import Scal
+from FADiff.fad.Matrices import Vect
+from FADiff.rev.Gradients import Scal
+from FADiff.rev.Matrices import Vect
 
